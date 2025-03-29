8103f8dbee29086acf249f7cfbb5a3e1473e8804
violettab <violettabatsura@gmail.com>
2025-03-30 00:07:23 +0300

    feat: solve partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -203,8 +203,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function (...args2) {
+    if (fn.length === args1.length) return fn(...args1);
+    return fn(...args1.concat(args2));
+  };
 }
 
 /**
