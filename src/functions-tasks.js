32da1a2fc517e3c1e04ea57aee3f87c97928bda4
violettab <violettabatsura@gmail.com>
2025-03-29 23:10:39 +0300

    feat: solve task retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -141,8 +141,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  let functionAttempts = attempts;
+  return function repeat() {
+    try {
+      func();
+    } catch {
+      functionAttempts -= 1;
+      if (functionAttempts > 0) {
+        repeat();
+      }
+    }
+    return func();
+  };
 }
 
 /**
